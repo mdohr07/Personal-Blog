6f7ad023232f85d42d62f3ab0498ed73796c8cdb
mirpx <mdohr07@gmail.com>
2024-06-16 15:34:22 +0200

    still trying

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,5 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://mdohr07.netlify.app',
-  integrations: [
-    sitemap({
-      filter: (page) =>
-        page !== 'https://mdohr07.netlify.app/2d' &&
-        page !== 'https://mdohr07.netlify.app/3d' &&
-        page !== 'https://mdohr07.netlify.app/ueber' &&
-        page !== 'https://mdohr07.netlify.app/blog',
-    }),
-  ],
+  integrations: [sitemap()],
 });
