17ea8669fc9b7669db901a9e85718d89d8befed7
mirpx <mdohr07@gmail.com>
2024-06-16 11:14:10 +0200

    Will the RSS Feed finally work?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://mdohr07.netlify.app', 
+  });
